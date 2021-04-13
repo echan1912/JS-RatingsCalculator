9f9527507b1d8e88902dc36e29850ce82a670233
echan1912 <echan1912@gmail.com>
2021-04-13 12:18:18 +0800

    add

--- js/ratings.js
+++ js/ratings.js
@@ -1,13 +1,13 @@
 const collect_ratings = () => {
     const ratings = { 'count' : 0, 'sum': 0, 'average': 0};
     let rating = 0;
-    const elements = document.querySelectorAll('rating');
+    const elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
-        element.id.replace('star', '');
+        rating = parseInt(element.id.replace('star', ''));
         ratings.sum += parseInt(element.value);
         parseInt(element.value)*ratings.sum;
     });
-    if (ratings.sum != 0) {
+    if (ratings.sum !== 0) {
         ratings.average = ratings.sum/ratings.count;
 
     }
@@ -18,4 +18,4 @@ document.addEventListener('change', () => {
     const ratings = collect_ratings();
     document.querySelector('#average').value = ratings.average.toFixed(2);
 
-})
+});
